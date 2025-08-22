fb885c2a340cf82efd923924d53074e1c609c577
Jason Rashaad <jasonrashaad@whatcomesnextllc.ai>
2025-08-22 05:09:17 -0400

    Redirect until I fix this. ~ JR

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     // Serve showcase at root, and pretty URL for DITL
     return [
       { source: "/", destination: "/showcase.html" },
+      { source: "/home", destination: "/showcase.html" },
       { source: "/ditl", destination: "/ditl.html" },
       { source: "/founderspage", destination: "/founderspage.html" },
     ];
